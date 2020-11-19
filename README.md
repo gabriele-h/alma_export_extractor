2e6bf6a26df7aa8550da59db91c2c4f5d17d3fce
Gabriele Höfler <gabriele.hoefler@univie.ac.at>
2020-11-19 15:35:10 +0000

    Switch from Metalib to Alma BIB

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Verwendung
 
-python xml\_extractor.py input.xml output.tsv
+python xml\_extractor.py input.xml output.csv 100,73308w,73308v
+
+Wenn Indikatoren irrelevant sind, beide(!) durch Asterisk ersetzen:
+
+python xml\_extractor.py input.xml output.csv 245\*\*a,245\*\*b
+
+Wichtig ist, dass bei der Angabe der Kategorien keine Leerzeichen enthalten sind.
--- xml_extract.py
+++ xml_extract.py
@@ -1,10 +1,11 @@
 """
 Sehr spezifischer Use-Case: Aus einem XML, das MetaLib zwecks Migration von einem Server zum andern anbietet,
-sollen bestimmte Daten in eine TSV-Datei übernommen werden, welche dann nach Excel importiert werden kann.
+sollen bestimmte Daten in eine CSV-Datei übernommen werden, welche dann nach Excel importiert werden kann.
 """
 
 from os import sys
 from xml.etree import ElementTree as etree
+from xml.etree.ElementTree import Element
 
 try:
     xml_dateipfad = sys.argv[1]
@@ -13,147 +14,79 @@ except IndexError:
     sys.exit(1)
 
 try:
-    tsv_dateipfad = sys.argv[2]
+    csv_dateipfad = sys.argv[2]
 except IndexError:
     print("Keine Output-Datei? Sollte Dateipfad zum TSV als zweiten Parameter nach Skriptnamen angeben.")
     sys.exit(1)
 
-ns = {'all': "http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd",
-      'xsi': "http://www.w3.org/2001/XMLSchema-instance"}
+try:
+    list_of_fields = sys.argv[3]
+except IndexError:
+    print("Keine Liste an Kategorien übergeben? Bitte als dritten Parameter angeben.")
 
 xml_etree = etree.parse(xml_dateipfad)
 
-with open(tsv_dateipfad, 'w+', encoding="utf-8") as tsv_datei:
-    tsv_header = []
-    tsv_data_lines = []
-    counters = {}
-    for ku in xml_etree.findall('all:knowledge_unit', ns):
-        for record in ku.findall('all:record', ns):
-            controlfields = record.findall('all:controlfield', ns)
-            local_counters = {}
-            for controlfield in controlfields:
-                controlfield_tag = controlfield.attrib['tag']
 
-                try:
-                    local_counters[controlfield_tag]
-                except KeyError:
-                    local_counters[controlfield_tag] = 1
-                else:
-                    local_counters[controlfield_tag] += 1
+def parse_record(csv_header: list, xml: Element) -> list:
 
-                try:
-                    counters[controlfield_tag]
-                except KeyError:
-                    counters[controlfield_tag] = 1
-                else:
-                    if local_counters[controlfield_tag] > counters[controlfield_tag]:
-                        counters[controlfield_tag] = local_counters[controlfield_tag]
+    csv_row = []
 
-                if controlfield_tag not in tsv_header:
-                    tsv_header.append(controlfield_tag)
-            for datafield in record.findall('all:datafield', ns):
-                datafield_tag = datafield.attrib['tag']
+    for field in csv_header:
 
-                try:
-                    datafield_ind1 = datafield.attrib['ind1']
-                except KeyError:
-                    datafield_ind1 = None
+        if field[0:2] == "00" and int(field[2]) <= 9:
+            field_type = "controlfield"
+        elif field == "leader":
+            field_type = "controlfield"
+        else:
+            field_type = "datafield"
 
-                try:
-                    datafield_ind2 = datafield.attrib['ind2']
-                except KeyError:
-                    datafield_ind2 = None
-
-                for subfield in datafield.findall('all:subfield', ns):
-                    try:
-                        subfield_code = subfield.attrib['code']
-                    except KeyError:
-                        print('No subfield for datafield?')
-                    else:
-                        datafield_description = datafield_tag
-                        if datafield_ind1:
-                            datafield_description += datafield_ind1
-                        if datafield_ind2:
-                            datafield_description += datafield_ind2
-                        if subfield_code:
-                            datafield_description += subfield_code
-                        if datafield_description not in tsv_header:
-                            tsv_header.append(datafield_description)
-
-                        try:
-                            local_counters[datafield_description]
-                        except KeyError:
-                            local_counters[datafield_description] = 1
-                        else:
-                            local_counters[datafield_description] += 1
-
-                        try:
-                            counters[datafield_description]
-                        except KeyError:
-                            counters[datafield_description] = 1
-                        else:
-                            if local_counters[datafield_description] > counters[datafield_description]:
-                                counters[datafield_description] = local_counters[datafield_description]
-
-    for ku in xml_etree.findall('all:knowledge_unit', ns):
-        tsv_line = {}
-        tsv_header.sort()
-        if ku.find('all:record', ns):
-            for field_type in tsv_header:
-                content = []
-
-                if counters[field_type] > 1:
-                    iterate = True
-                else:
-                    iterate = False
-
-                if len(field_type) == 3:
-                    controlfield_tag = field_type
-                    for controlfield in ku.findall('all:record/all:controlfield', ns):
-                        if controlfield.attrib['tag'] == controlfield_tag:
-                            field_type_content = controlfield.text
-                            if iterate:
-                                content.append(field_type_content)
-                elif len(field_type) == 6:
-                    datafield_tag = field_type[0:3]
-                    datafield_ind1 = field_type[3]
-                    datafield_ind2 = field_type[4]
-                    subfield_code = field_type[5]
-                    for datafield in ku.findall('all:record/all:datafield', ns):
-                        if datafield.attrib['tag'] == datafield_tag \
-                            and datafield.attrib['ind1'] == datafield_ind1 \
-                                and datafield.attrib['ind2'] == datafield_ind2:
-                            for subfield in datafield.findall('all:subfield', ns):
-                                if subfield.attrib['code'] == subfield_code:
-                                    field_type_content = subfield.text
-                                    if iterate:
-                                        content.append(field_type_content)
-                try:
-                    tsv_line[field_type] = field_type_content
-                    if iterate:
-                        tsv_line[field_type] = ';'.join(content)
-                except NameError:
-                    tsv_line[field_type] = ""
-                field_type_content = ''
-            categories = []
-            for category in ku.findall('all:category', ns):
-                main_found = category.findall('all:main', ns)
-                sub_found = category.findall('all:sub', ns)
-                for main, sub in zip(main_found, sub_found):
-                    categories.append(main.text + ': ' + sub.text)
-            tsv_line['category'] = ', '.join(categories)
-            try:
-                tsv_line_string = '\t'.join(tsv_line.values())
-            except TypeError:
-                print('Knowledge Unit kaputt?')
+        if len(field) == 3:
+            if field_type == "controlfield":
+                xpath = f"{field_type}[@tag='{field}']"
             else:
-                tsv_data_lines.append(tsv_line_string)
-    tsv_header.append('categories')
-    tsv_header_string = '\t'.join(tsv_header)
-    tsv_datei.write(tsv_header_string)
-    tsv_datei.write('\n')
-    n = 1
-    for line in tsv_data_lines:
-        tsv_datei.write(line)
-        tsv_datei.write('\n')
-        n = n+1
+                xpath = f"{field_type}[@tag='{field}']/subfield"
+        elif len(field) == 5:
+            if field[3] == '*' and field[4] == '*':
+                xpath = f"{field_type}[@tag='{field[0:3]}']/subfield"
+            else:
+                xpath = f"{field_type}[@tag='{field[0:3]}' and ind1='{field[3]}' and ind2='{field[4]}']/subfield"
+        elif len(field) == 6:
+            if field[3] == '*' and field[4] == '*':
+                xpath = f"{field_type}[@tag='{field[0:3]}']/subfield[@code='{field[5]}']"
+            else:
+                xpath = f"{field_type}[@tag='{field[0:3]}' and ind1='{field[3]}' and ind2='{field[4]}']/subfield[@code='{field[5]}']"
+        else:
+            print("Given list of fields did not meet expectations.")
+
+        # print(xpath)
+
+        found_fields = xml.findall(xpath)
+        found_fields_concat = []
+        for field in found_fields:
+            try:
+                code = field.attrib['code']
+                found_fields_concat.append('$$' + code + field.text)
+            except KeyError:
+                found_fields_concat.append(field.text.replace('"', '""'))
+
+        csv_row.append('"' + ''.join(found_fields_concat) + '"')
+
+    return csv_row
+
+
+with open(csv_dateipfad, 'w+', encoding="utf-8") as csv_datei:
+    csv_datei.write('\ufeff')
+
+    csv_header = list_of_fields.split(',')
+
+    csv_content = []
+
+    for record in xml_etree.findall('.//record'):
+        csv_row = parse_record(csv_header, record)
+        # print(csv_row)
+        csv_content.append(csv_row)
+
+    csv_datei.write('"' + '";"'.join(csv_header) + '"\n')
+
+    for line in csv_content:
+        csv_datei.write(";".join(line) + '\n')
